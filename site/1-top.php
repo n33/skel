5134a8b028332f5d3e6aafa64b2b73b5890b232d
aj <aj@n33.co>
2013-06-03 23:42:02 -0500

    Core: Removed site/updateSite (now maintained separately

--- site/1-top.php
+++
@@ -1,7 +0,0 @@
-<section id="top" class="container">
-	<h1>skel.js</h1>
-	<p>A lightweight frontend framework for building responsive sites and apps.</p>
-	<div class="actions">
-		<a href="files/skel.min.js" class="button huge icon icon-download-alt">Download (19kb)</a>
-	</div>
-</section>
