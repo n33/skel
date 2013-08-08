f23f58065912eaca10bd4392214af176646e6da0
aj <aj@n33.co>
2013-08-07 19:15:34 -0500

    Added new API calls (lock, unlock, getLock, canUse)

--- src/skel.js
+++ src/skel.js
@@ -29,6 +29,7 @@ var skel = (function() { var _ = {
 			boxModel: null,				// Sets the CSS box model (border, content, margin, padding)
 			useOrientation: false,			// If true, device orientation will be factored into viewport width calculations
 			useRTL: false,				// If true, make adjustments for right-to-left (RTL) languages
+			pollOnLock: false,			// If true, poll after locking instead of refreshing (= good for testing locally)
 			containers: 960,			// Width of container elements
 			containerUnits: false,			// (deprecated) Container units (px, pt, %, vw)
 			grid: {					// Grid
@@ -48,6 +49,7 @@ var skel = (function() { var _ = {
 		isConfigured: false,				// Are we configured?
 		isInit: false,					// Are we initialized?
 		IEVersion: 99,					// Current IE version
+		lockState: null,				// Current lock state
 		stateId: '',					// Current state ID
 		me: null,					// My <script> element
 		breakpoints: [],				// Breakpoints
@@ -69,6 +71,7 @@ var skel = (function() { var _ = {
 	// Data
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+		lsc: '_skel_lock',			// Lock state cookie name (don't change this)
 		sd: ' ',					// State ID delimiter (don't change this)
 		css: {						// CSS code blocks (reset, normalize, various parts of the grid system)
 			r: 'html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video{margin:0;padding:0;border:0;font-size:100%;font:inherit;vertical-align:baseline}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block}body{line-height:1}ol,ul{list-style:none}blockquote,q{quotes:none}blockquote:before,blockquote:after,q:before,q:after{content:\'\';content:none}table{border-collapse:collapse;border-spacing:0}body{-webkit-text-size-adjust:none}',
@@ -260,11 +263,59 @@ var skel = (function() { var _ = {
 				// Divide by pixel ratio
 					w = w / r;
 
-				// Set values
-					_.values['viewportWidth'] = w;
-					_.values['devicePixelRatio'] = r;
-
 				return w;
+
+			},
+			
+		/* API  */
+
+			// Unlocks width
+			unlock: function() {
+				
+				// Clear the lock state
+					_.lockState = null;
+					document.cookie = _.lsc + '=;expires=Thu, 1 Jan 1970 12:00:00 UTC; path=' + window.location.pathname;
+					console.log('api: unlocking width');
+				
+				// Poll or reload
+					if (_.config.pollOnLock)
+						_.poll();
+					else
+						window.location.reload();
+
+			},
+			
+			// Locks to a given width
+			// Args: integer w (Width)
+			lock: function(w) {
+
+				// Set the lock state
+					_.lockState = w;
+					document.cookie = _.lsc + '=' + w + ';expires=Thu, 1 Jan 2077 12:00:00 UTC; path='  + window.location.pathname;
+					console.log('api: locking width to ' + w);
+				
+				// Poll or reload
+					if (_.config.pollOnLock)
+						_.poll();
+					else
+						window.location.reload();
+
+			},
+
+			// Gets the current lock state
+			// Returns: integer (Lock state value)
+			// Returns: null (Not locked)
+			getLock: function() {
+
+				return _.lockState;
+
+			},
+			
+			// Determines if we're locked to a width
+			// Returns: bool (Lock state)
+			isLocked: function() {
+
+				return !!(_.lockState);
 
 			},
 			
@@ -284,6 +335,15 @@ var skel = (function() { var _ = {
 
 				return (_.indexOf(_.values['lastStateId'], _.sd + k) !== -1);
 
+			},
+			
+			// Determines if a given breakpoint applies to the current viewport width
+			// Args: string k (Breakpoint ID)
+			// Returns: bool (Breakpoint applicability)
+			canUse: function(k) {
+
+				return (_.breakpoints[k] && (_.breakpoints[k].test)(_.getViewportWidth()));
+			
 			},
 			
 			// Gets an internal state value
@@ -396,7 +456,7 @@ var skel = (function() { var _ = {
 
 				_.bind('stateChange', f); 
 				
-				// If skel's already been initialized and we're just now binding this event,
+				// If skelJS has already been initialized and we're just now binding this event,
 				// we're late to the game so manually fire it once.
 					if (_.isInit)
 						(f)();
@@ -580,8 +640,16 @@ var skel = (function() { var _ = {
 			
 				var k, w, newStateId = '';
 				
-				// Calculate width
-					w = _.getViewportWidth();
+				// If we're locked to a width, use that as the viewport width
+					if (_.lockState)
+						w = _.lockState;
+				// Otherwise, calculate the actual viewport width
+					else
+						w = _.getViewportWidth();
+
+				// Set values
+					_.values['viewportWidth'] = w;
+					_.values['devicePixelRatio'] = _.getDevicePixelRatio();
 				
 				// Determine new state
 					_.iterate(_.breakpoints, function(k) {
@@ -1316,6 +1384,28 @@ var skel = (function() { var _ = {
 							};
 
 			},
+
+			// Initializes the API
+			initAPI: function() {
+				
+				var x;
+			
+				// Init lock state
+					x = document.cookie.split(';');
+					
+					_.iterate(x, function(i) {
+						var y = x[i].split('=');
+						
+						if (y[0] == _.lsc)
+						{
+							_.lockState = y[1];
+							return;
+						}
+					});
+					
+					console.log('api: width lock is ' + _.lockState);
+
+			},			
 			
 			// Initializes skelJS
 			init: function(config, pluginConfig) {
@@ -1327,6 +1417,9 @@ var skel = (function() { var _ = {
 
 				// Init utility methods
 					_.initUtilityMethods();
+
+				// Init API
+					_.initAPI();
 
 				// Init config
 					if (config)
